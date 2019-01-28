76e14215b3c2f628d8f82489f4c4ef1f63a178f7
christopher mochizuki <mochic808@gmail.com>
2019-01-27 21:36:22 -0800

    remove unused comment

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.send();
 });
